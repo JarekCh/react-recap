f8b45830222a4c0885a475a0e8d1c93cb0d2cdef
Jaroslaw Charchula <charchula@gmail.com>
2022-08-09 21:53:46 +0200

    import fix

--- slider/src/App.js
+++ slider/src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
 import { FaQuoteRight } from "react-icons/fa";
 import data from "./data";
-import { positions } from "@mui/system";
+
 function App() {
   const [people, setPeople] = useState(data);
   const [index, setIndex] = useState(0);
